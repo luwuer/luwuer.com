a4ab917f9400bdeee000b765efd05b8f2cf1f5c1
luwuer <html6@foxmail.com>
2018-09-03 20:17:43 +0800

    添加server目录

---
+++ server/config.js
@@ -0,0 +1,14 @@
+const config = {
+
+  port: 3000,
+
+  database: {
+    DATABASE: 'koa_demo',
+    USERNAME: 'root',
+    PASSWORD: 'abc123',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
