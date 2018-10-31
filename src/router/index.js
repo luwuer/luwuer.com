f26c0d49155989bb4364448b7908d8bcd3b1fd5f
luwuer <html6@foxmail.com>
2018-10-31 20:16:39 +0800

    add HUNDSUN community login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Homepage from '@/pages/index/components/homepage'
+import Test from '@/pages/index/pages/test'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'homepage',
       component: Homepage
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test
     }
   ]
 })
